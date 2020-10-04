c185849f151fc83db9ff2f62624708755a6df5ec
Will Maginn <wj.maginn@gmail.com>
2020-10-04 20:51:45 +0100

    Added test lib and first testing, updated readme with more exit codes

---
+++ aws-auth/tests/cli_test.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+# file: examples/party_test.sh
+
+testAWSCheckOptionDoesNotExist() {
+    ../aws-auth -c /usr/does-not-exist >> $stdoutF 2>&1
+    assertEquals 10 $?
+}
+
+testAWSCheckOptionDoesExist() {
+    ../aws-auth -c ls >> $stdoutF 2>&1
+    assertEquals 13 $?
+}
+
+testAWSCheckOptionEmpty() {
+    ../aws-auth -c >> $stdoutF 2>&1
+    assertEquals 14 $?
+}
+
+testAWSCheckENVDoesNotExist() {
+    export AWS_AUTH_CLI_PATH=""
+    ../aws-auth 123456 >> $stdoutF 2>&1
+    assertEquals 10 $?
+}
+
+testAWSCheckENVExist() {
+    export AWS_AUTH_CLI_PATH="ls"
+    ../aws-auth 123456 >> $stdoutF 2>&1
+    assertEquals 13 $?
+}
+
+tearDown() {
+    unset AWS_AUTH_CLI_PATH
+}
+
+oneTimeSetUp() {
+    outputDir="${SHUNIT_TMPDIR}/output"
+    mkdir "${outputDir}"
+    stdoutF="${outputDir}/stdout"
+    stderrF="${outputDir}/stderr"
+    testDir="${SHUNIT_TMPDIR}/aws-auth"
+}
+
+# oneTimeTearDown() {
+    # echo "Test logs: $testDir"
+    # rm -fr "${testDir}"
+# }
+
+# load shunit2
+shunit2=`which shunit2`
+. shunit2
