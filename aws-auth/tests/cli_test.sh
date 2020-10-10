ddaaeedb89bba0343d977f8675ad81c0e2e8150a
Will Maginn <wj.maginn@gmail.com>
2020-10-10 22:50:01 +0100

    Updated test as they should be passing

--- aws-auth/tests/cli_test.sh
+++ aws-auth/tests/cli_test.sh
@@ -2,30 +2,30 @@
 # file: tests/cli_test.sh
 
 testAWSCheckOptionDoesNotExist() {
-    ../aws-auth -c /usr/does-not-exist >> $stdoutF 2>&1
+    aws-auth/aws-auth -c /usr/does-not-exist >> $stdoutF 2>&1
     assertEquals 10 $?
 }
 
 testAWSCheckOptionDoesExist() {
-    ../aws-auth -c ls >> $stdoutF 2>&1
-    assertEquals 13 $?
+    aws-auth/aws-auth -c ls >> $stdoutF 2>&1
+    assertEquals 1 $?
 }
 
 testAWSCheckOptionEmpty() {
-    ../aws-auth -c >> $stdoutF 2>&1
+    aws-auth/aws-auth -c >> $stdoutF 2>&1
     assertEquals 14 $?
 }
 
 testAWSCheckENVDoesNotExist() {
     export AWS_AUTH_CLI_PATH=""
-    ../aws-auth 123456 >> $stdoutF 2>&1
+    aws-auth/aws-auth 123456 >> $stdoutF 2>&1
     assertEquals 10 $?
 }
 
 testAWSCheckENVExist() {
     export AWS_AUTH_CLI_PATH="ls"
-    ../aws-auth 123456 >> $stdoutF 2>&1
-    assertEquals 13 $?
+    aws-auth/aws-auth 123456 >> $stdoutF 2>&1
+    assertEquals 1 $?
 }
 
 tearDown() {
@@ -46,5 +46,9 @@ oneTimeSetUp() {
 # }
 
 # load shunit2
-shunit2=`which shunit2`
-. shunit2
+sh2=`which shunit2`
+if [ $? -ne 0 ]; then
+    . sh2
+else
+    . shunit2
+fi
